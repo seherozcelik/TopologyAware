ee359579805a9e4f1dc8842115d1c2ab34c1d4f5
seher özçelik <seherozcelik@gmail.com>
2023-07-21 17:12:10 +0300

    Update loss.py

--- base/loss.py
+++ base/loss.py
@@ -6,8 +6,8 @@ class CrossEntropyLoss(nn.Module):
     def __init__(self):
         super(CrossEntropyLoss, self).__init__()
 
-    def forward(self, pred, golds):
-        gold = torch.FloatTensor(golds[0,:,:]).cuda()
+    def forward(self, pred, gold):
+        gold = torch.FloatTensor(gold[0,:,:]).cuda()
         pred = pred[0,0,:,:]
 
         loss = - gold * torch.log(pred + 1e-8) - (1-gold) * torch.log((1-pred) + 1e-8)
